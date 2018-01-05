10d096311515c3c14b8dcba82d5e07b9a75f1f08
Atif Mahadik <atif@phantom.us>
2018-01-05 11:31:51 -0800

    Adding Submitted 1st cut of the app

---
+++ CriticalStack_Intel_connector.py
@@ -0,0 +1,293 @@
+from CriticalStack import Intel
+from datetime import datetime
+from phantom.action_result import ActionResult
+import phantom.app as phantom
+from phantom.base_connector import BaseConnector
+from PhantomAPI import PhantomList
+
+
+class CriticalStack_Intel_connector(BaseConnector):
+
+    BANNER = "CriticalStack Connector"
+    TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+    CS_DATA_TYPES = {'ip': 'ADDR', 'domain': 'DOMAIN', 'hash': 'FILE_HASH'}
+    CS_LAST_UPDATED_LIST = 'CriticalStack-LastUpdated'
+
+    def initialize(self):
+        return phantom.APP_SUCCESS
+
+    def finalize(self):
+        return
+
+    def handle_exception(self, exception_object):
+        """All the code within BaseConnector::_handle_action is within a 'try:
+        except:' clause. Thus if an exception occurs during the execution of
+        this code it is caught at a single place. The resulting exception
+        object is passed to the AppConnector::handle_exception() to do any
+        cleanup of it's own if required. This exception is then added to the
+        connector run result and passed back to spawn, which gets displayed
+        in the Phantom UI.
+        """
+
+        return
+
+    def _initialize_critical_stack(self, config):
+        cs = Intel(
+            config.get('criticalStackServer'),
+            config.get('sshUser'),
+            config.get('sshPassword'),
+            apiKey=config.get('criticalStackApiKey'),
+            file_loc=config.get('criticalStackFileLoc')
+        )
+
+        return cs
+
+    def _initialize_phantom_list(self, config):
+        phant = PhantomList(
+            verify_cert=config.get("verifyPhantomServerCert")
+        )
+
+        return phant
+
+    def _pluralize(self, cs_type):
+        return {
+            'IP': 'IPs',
+            'DOMAIN': 'domains',
+            'HASH': 'hashes'
+        }[cs_type.upper()] or 'unknown'
+
+    def _set_error(self, action_result, message, err):
+        action_result.set_status(
+            phantom.APP_ERROR,
+            message,
+            err.message
+        )
+        return action_result.get_status()
+
+    def _update_needed(self, phant, prefix):
+
+        last_updated_list = phant.get_list(self.CS_LAST_UPDATED_LIST)
+        last_updated_record = []
+        update_needed = True
+
+        if(
+            'content' in last_updated_list
+            and len(last_updated_list['content'][0]) < 2
+        ):
+            raise IndexError(
+                self.CS_LAST_UPDATED_LIST + ' - list is malformed. Expected '
+                'Column1-Prefix, Column2-Last Updated Date.'
+            )
+        elif 'content' in last_updated_list:
+            last_updated_record = [
+                record for record in last_updated_list['content']
+                if record[0] == prefix
+            ]
+
+        if len(last_updated_record) > 0:
+
+            try:
+                t1 = datetime.strptime(
+                    last_updated_record[0][1],
+                    self.TIME_FORMAT
+                )
+            except ValueError as err:
+                raise ValueError(
+                    'Second column of list - ' + self.CS_LAST_UPDATED_LIST
+                    + ' - does not represent a date time in '
+                    + self.TIME_FORMAT + ' format. Details: ' + err.message
+                )
+
+            now = datetime.now()
+
+            if((now-t1).total_seconds() < 1800):
+                update_needed = False
+
+        return update_needed
+
+    def _update_phantom_lists(self, cs, phant, prefix):
+        cs.run_cs_command('pull')
+        response = cs.get_cs_list_data()
+
+        for cs_type in list(self.CS_DATA_TYPES.values()):
+            cs_list = [
+                record for record in response
+                if 'Intel::' + cs_type in record
+            ]
+            phant.create_list(
+                [line.split('\t') for line in cs_list],
+                prefix + '-' + cs_type
+            )
+
+        last_updated_list = phant.get_list(self.CS_LAST_UPDATED_LIST)
+
+        now = datetime.now().strftime(self.TIME_FORMAT)
+
+        if 'content' in last_updated_list:
+            rows_to_update = phant.search_list(last_updated_list, 0, prefix)
+            if len(rows_to_update) > 0:
+                for row_num in rows_to_update:
+                    if len(last_updated_list['content'][row_num]) < 2:
+                        raise IndexError(
+                            self.CS_LAST_UPDATED_LIST
+                            + ' - list is malformed. '
+                            'Expected Column1-Prefix, Column2-Last Updated '
+                            'Date.'
+                        )
+                    else:
+                        last_updated_list['content'][row_num][1] = now
+            else:
+                last_updated_list['content'].append([prefix, now])
+
+            phant.create_list(
+                last_updated_list['content'],
+                self.CS_LAST_UPDATED_LIST
+            )
+        else:
+            phant.create_list([[prefix, now]], self.CS_LAST_UPDATED_LIST)
+
+        return
+
+    def _perform_action(self, param, cs_type):
+        config = self.get_config()
+        prefix = config.get('criticalStackPrefix')
+
+        action_result = self.add_action_result(ActionResult(dict(param)))
+
+        cs = self._initialize_critical_stack(config)
+        phant = self._initialize_phantom_list(config)
+
+        try:
+            update_needed = self._update_needed(
+                phant,
+                prefix
+            )
+        except Exception as err:
+            return self._set_error(action_result, 'Phantom List Error 1', err)
+
+        if update_needed:
+            try:
+                self._update_phantom_lists(cs, phant, prefix)
+            except Exception as err:
+                return self._set_error(
+                    action_result,
+                    'Phantom List Error 2',
+                    err
+                )
+
+        try:
+            phantom_list = phant.get_list(
+                prefix + '-' + self.CS_DATA_TYPES[cs_type]
+            )
+        except Exception as err:
+            return self._set_error(action_result, 'Phantom List Error 3', err)
+
+        found_rows = []
+
+        if('content' in phantom_list):
+
+            try:
+                found_rows = phant.search_list(phantom_list, 0, param[cs_type])
+            except Exception as err:
+                return self._set_error(
+                    action_result,
+                    'Phantom List Error 4',
+                    err
+                )
+            else:
+                message = (
+                    str(len(phantom_list['content'])) + ' '
+                    + self._pluralize(cs_type) + ' scanned.'
+                )
+
+        else:
+            message = (
+                'No ' + self._pluralize(cs_type) + ' exist in ' + prefix
+                + '-' + self.CS_DATA_TYPES[cs_type]
+            )
+
+        summary = {
+            'detected_' + cs_type + '_count': str(len(found_rows)),
+            'lists_updated': update_needed,
+            'list_prefix': prefix,
+            'additional_details': message
+        }
+
+        results = {'sources': []}
+
+        if len(found_rows) > 0:
+            results = {
+                'sources':
+                [
+                    {
+                        cs_type: phantom_list['content'][row][0],
+                        'source': phantom_list['content'][row][2]
+                    } for row in found_rows
+                ]
+            }
+
+        action_result.update_summary(summary)
+        action_result.add_data(results)
+        action_result.set_status(phantom.APP_SUCCESS)
+
+        return action_result.get_status()
+
+    def _test_connectivity(self, param):
+        config = self.get_config()
+
+        cs = self._initialize_critical_stack(config)
+        phant = self._initialize_phantom_list(config)
+
+        try:
+            cs.connect_to_cs(verify_connect=True)
+        except Exception as err:
+            return self.set_status_save_progress(
+                phantom.APP_ERROR,
+                'Error connecting to Critical Stack. Details: ' + err.message
+            )
+
+        try:
+            phant.get_list('testConnectionList')
+        except Exception as err:
+            return self.set_status_save_progress(
+                phantom.APP_ERROR,
+                config.get('phantomApiKey')
+                + 'Error connecting to Phantom API. Details: ' + err.message
+            )
+
+        return self.set_status_save_progress(
+            phantom.APP_SUCCESS,
+            (
+                'Successfully logged into - '
+                + config.get('criticalStackServer')
+                + ' and ran - sudo critical-stack-intel list. '
+                'Succesfully connected to Phantom API.'
+            )
+        )
+
+    def handle_action(self, param):
+
+        action_id = self.get_action_identifier()
+
+        supported_actions = {
+            "test connectivity": self._test_connectivity,
+            "ip reputation": self.ip_reputation,
+            "domain reputation": self.domain_reputation,
+            "file reputation": self.file_reputation
+        }
+
+        run_action = supported_actions[action_id]
+
+        return run_action(param)
+
+    def ip_reputation(self, param):
+
+        return self._perform_action(param, 'ip')
+
+    def domain_reputation(self, param):
+
+        return self._perform_action(param, 'domain')
+
+    def file_reputation(self, param):
+
+        return self._perform_action(param, 'hash')
---
+++ CriticalStack_Intel_view.py
@@ -0,0 +1,182 @@
+import json
+
+
+def all_results(provides, all_results, context):
+
+    json_view = context['QS'].get('force_jsonview', ['False'])
+
+    if not provides:
+        provides = 'Query Value'
+
+    if json_view[0] == 'true':
+        return _get_json_data(all_results, context, provides)
+    else:
+        return _get_data(all_results, context, provides)
+
+
+def _get_data(all_results, context, provides):
+    summary_results = []
+    body_color = '#303741'
+
+    cs_type = provides
+    inner_id = 0
+
+    for summary, action_results in all_results:
+        if not action_results:
+            continue
+        for result in action_results:
+            parameter = result.get_param()
+            result_summary = result.get_summary()
+            data = result.get_data()
+            row_id = str(inner_id)
+            accordion_toggle = ''
+
+            if(
+                len(data) > 0
+                and 'sources' in data[0]
+                and len(data[0]['sources']) > 0
+            ):
+                accordion_toggle = 'accordion-toggle'
+
+            if result_summary and parameter:
+
+                summary_results.append((
+                    data,
+                    row_id,
+                    accordion_toggle,
+                    (
+                        parameter.get(provides)
+                        or parameter.get('hash')
+                        or parameter.get('ip')
+                        or parameter.get('domain')
+                    ),
+                    (
+                        result_summary.get('detected_'+provides+'_count')
+                        or result_summary.get('detected_hash_count')
+                        or result_summary.get('detected_ip_count')
+                        or result_summary.get('detected_domain_count')
+                    ),
+                    result_summary.get('lists_updated'),
+                    result_summary.get('list_prefix'),
+                    result_summary.get('additional_details')
+                ))
+
+            inner_id = inner_id + 1
+
+    context['summary_results'] = summary_results
+    context['cs_type'] = cs_type
+    context['body_color'] = body_color
+
+    return 'CriticalStack_Intel_template.html'
+
+
+def _get_json_data(all_results, context, provides):
+    data = []
+    inner_data = []
+    json_html = ''
+    for summary, action_results in all_results:
+        if action_results:
+            for result in action_results:
+                inner_data.append(
+                    {
+                        'results':
+                        {
+                            'summary': result.get_summary(),
+                            'parameters': result.get_param(),
+                            'data': result.get_data()
+                        }
+                    }
+                )
+
+    print(str(inner_data))
+    if len(inner_data) > 0:
+        json_html = json_html + _json_treeify(inner_data, None, json_html, 0, '')
+
+    json_html = (
+                '<div class="treeview" onwidgetload="init_treeview_widget(this);" '
+                'style="padding-left: 5px;">'
+                '<div class="treewidget" onclick="toggle(event, this);">ActionResults'
+            ) + json_html + '</div></div>'
+
+    context['json_data'] = json_html
+    return 'CriticalStack_Intel_json_template.html'
+
+def _json_treeify(a, parent, htmlstring, index, list_name):
+
+        if isinstance(a, list) or isinstance(a, dict):
+            if isinstance(a, list):
+                for idx, b in enumerate(a):
+                    htmlstring = htmlstring + (
+                        '<div class="treeitem treewidget">'
+                        + (
+                            (
+                                '<span class="keyname">' + list_name + '&nbsp;'
+                                '</span>'
+                            )
+                            if list_name != '' else ''
+                        )
+                        + '<span class="type-indicator">['
+                        + str(idx)
+                        + ']</span>'
+                        + '&nbsp;&nbsp;'
+                    )
+                    htmlstring = (
+                        _json_treeify(b, a, htmlstring, idx, '')
+                        + '</div>'
+                    )
+
+                return htmlstring
+
+            elif isinstance(a, dict):
+                if not(isinstance(parent, list) or isinstance(parent, dict)):
+                    htmlstring = htmlstring + (
+                        '<div class="treeitem treewidget">'
+                        + str(parent)
+                        + '&nbsp;'
+                    )
+
+                htmlstring = htmlstring + (
+                    '<span class="type-indicator">{' + str(len(a)) + '}</span>'
+                )
+
+                for idx, b in enumerate(a):
+                    htmlstring = _json_treeify(b, a, htmlstring, index, '')
+
+                if(
+                    not(isinstance(parent, list) or isinstance(parent, dict))
+                    and htmlstring
+                ):
+                    return(htmlstring + '</div>')
+                else:
+                    return(htmlstring)
+        else:
+            if isinstance(parent, dict):
+                if isinstance(parent[a], dict):
+                    return _json_treeify(
+                        parent[a],
+                        a,
+                        htmlstring,
+                        index,
+                        ''
+                    )
+                elif isinstance(parent[a], list):
+                    return _json_treeify(
+                        parent[a],
+                        a,
+                        htmlstring,
+                        0,
+                        a
+                    )
+                else:
+                    return htmlstring + (
+                        '<div class="treeitem treedata">'
+                        '<span class="keyname">' + str(a) + '</span>&nbsp;'
+                        '<span class="treedata" data-contains="[]">'
+                        + str(parent[a]) + '</span></div>'
+                    )
+            else:
+                return htmlstring + (
+                    '<div class="treeitem treedata">'
+                    '<span class="treedata" data-contains="[]">'
+                    + str(parent[a]) + '</span></div>'
+                )
